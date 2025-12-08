053da06c295226be23d693b351979cab0021c546
David Walker <dbwalker0min@gmail.com>
2025-12-08 13:53:23 -0800

    Day 7 part 2 complete

--- advent_of_code/year_2025/day_07/day_07.py
+++ advent_of_code/year_2025/day_07/day_07.py
@@ -1,6 +1,5 @@
 from io import TextIOBase
 from itertools import count
-from time import monotonic_ns
 
 class Manifold:
     _translation_table = str.maketrans('^S', '.|')
@@ -23,8 +22,9 @@ class Manifold:
         # this is the number of tachyon splits
         self._nsplit = 0
 
-        # this keeps track of all the paths
-        self._paths: set[tuple[int]] = set(tuple())
+        # this keeps track of all the number paths for each column entry.
+        # This is updated each step
+        self._npaths: set[int] = [0]*len(self._manifold[0])
 
         # this is the current line I'm working on
         self._step = 0
@@ -38,45 +38,32 @@ class Manifold:
         cur_row: list[str] = list(self._manifold[self._step])
         next_row = list(self._manifold[self._step + 1])
 
-        # allocate the new paths
-        new_paths: set[tuple[int]] = set()
-
         # if there are no splitters in `next_row`, simply propagate
         if all(c == '.' for c in next_row):
             # this is only propagation. Simply copy `cur_row` to `next_row` eliminating splitters.
             next_row = (c.translate(self._translation_table) for c in cur_row)
             if 'S' in cur_row:
-                # build a path to the element. It is a tuple of 1 element
-                x = tuple([cur_row.index('S')])
-                self._paths.add(tuple([cur_row.index('S')]))
+                # There is exactly one way to get here
+                self._npaths[cur_row.index('S')] = 1
         else:
             for col, cr, nr in zip(count(0), cur_row, next_row):
 
                 if cr in '|S':
-                    # Find the paths that lead to the current position
-                    paths_to_me: set[tuple[int]] = list(p for p in self._paths if p[-1] == col)
-
                     # Tachyon found. Propagate it.
                     if nr == '^':
                         self._nsplit += 1
                         # Splitter found. Place tachyons on either side of the splitter
                         next_row[col - 1], next_row[col + 1] = '|', '|'
 
+                        # There are npaths ways to get to both the right and left branches
+                        self._npaths[col - 1] += self._npaths[col]
+                        self._npaths[col + 1] += self._npaths[col]
+                        self._npaths[col] = 0
 
-                        # add the left and right branches to the paths
-                        for p in paths_to_me:
-                            new_paths.add(p + tuple([col - 1]))
-                            new_paths.add(p + tuple([col + 1]))
                     else:
                         # Just propagate it
                         next_row[col] = '|'
-                        for p in paths_to_me:
-                            new_paths.add(p + tuple([col]))
-            
-            # save the new path list
-            self._paths = new_paths
-
-
+                        # the number of paths doesn't change
         
         # save the new next row
         self._manifold[self._step + 1] = ''.join(next_row)
@@ -91,17 +78,11 @@ class Manifold:
     
     def run_steps(self):
         for n in count():
-            print(f'Do step {n}')
-            t0 = monotonic_ns()
             if self.step(trace=False):
                 break
-            print(f'Step {n} took {(monotonic_ns() - t0)/1e9} s')
+
         if self._trace:
             self.print_trace()
-
-    def count_timelines(self) -> int:
-        return 0
-
 
     @property
     def get_splits(self):
@@ -111,4 +92,4 @@ class Manifold:
     @property
     def get_number_paths(self):
         """A simple getter for the number of paths"""
-        return len(self._paths)
+        return sum(self._npaths)
