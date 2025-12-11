289bc74306051db06a2779c888b35d9973291759
David Walker <dbwalker0min@gmail.com>
2025-12-10 18:19:18 -0800

    Still plugging away on Day 9. This is taking forever!

--- advent_of_code/year_2025/day_09/day_09.py
+++ advent_of_code/year_2025/day_09/day_09.py
@@ -1,20 +1,35 @@
 from io import TextIOBase
-from itertools import combinations
+from itertools import combinations, zip_longest
 from typing import assert_never
+from dataclasses import dataclass
 
-TUPLE2 = tuple[int, int]
 
-def parse_file(f: TextIOBase) -> set[TUPLE2]:
+@dataclass(frozen=True)
+class Tuple2:
+    x: int
+    y: int
+
+    def compute_area(self, other: "Tuple2"):
+        """Compute the area of the tuple agains the other one"""
+        return (abs(self.x - other.x) + 1) * (abs(self.y - other.y) + 1)
+
+
+@dataclass
+class Segment:
+    """The segment goes from a to b."""
+    a: Tuple2
+    b: Tuple2
+
+
+def parse_file(f: TextIOBase) -> set[Tuple2]:
     """Parse the file of 2-element tuples. A set is used to ignore any duplicates (if any exist)"""
-    result: set[TUPLE2] = set()
+    result: set[Tuple2] = set()
     for line in f:
-        line = line.rstrip('\n')
-
-        result.add(tuple(int(x) for x in line.split(',')))
+        line = line.rstrip("\n")
+        x, y = [int(x) for x in line.split(",")]
+        result.add(Tuple2(x, y))
     return result
 
-def compute_area(a: TUPLE2, b: TUPLE2) -> int:
-    return (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1)
 
 def largest_red_rectangle(f: TextIOBase) -> int:
     """Given the elements in the file, determine the largest red rectangle"""
@@ -24,28 +39,39 @@ def largest_red_rectangle(f: TextIOBase) -> int:
     # This is tractable; the actual problem has 496 lines and C(496, 2) = 122,760
     largest_area = 0
     for a, b in combinations(existing_tiles, 2):
-        largest_area = max(compute_area(a, b), largest_area)
+        largest_area = max(a.compute_area(b), largest_area)
 
     return largest_area
 
-def find_first(x: list[int], val: int):
-    """Find the indicies of `val` in the input list"""
+
+def find_first(x: list[Tuple2], val: Tuple2):
+    """Find the indicies of `val` in the input lists"""
     # This only works because I *know* that there'll be zero or one point
+
+    # try to find a point that's the same in x
     try:
-        return x.index(val)
+        return x.index(val[0])
     except ValueError:
-        return None
-        
+        # X not found. Try y
+        try:
+            return y.index(val[1])
+        except ValueError:
+            return None
 
-def largest_red_and_green(f: TextIOBase) -> int:
-    verticies = parse_file(f)
 
-    # split them by x and y coordinates
-    vx = [v[0] for v in verticies]
-    vy = [v[1] for v in verticies]
+def segment_horizontal(s: Segment) -> bool:
+    """Is the segment horizontal or vertical? If neither, an exception is thrown."""
+    if s[0][1] == s[1][1]:
+        return True
+    elif s[0][0] == s[1][0]:
+        return False
+    else:
+        assert False
 
-    shapes = list[list[TUPLE2]]
-    while vx:
+
+def get_shapes(verticies: list[Tuple2]) -> list[Tuple2]:
+    shapes: list[list[Tuple2]] = []
+    while verticies:
         # Take the vertext and try to build the shape around it
         # Note that there may not be any shape associated with it (singleton point)
         # Lines are considered shapes so a shape consists of at least two points
@@ -54,33 +80,80 @@ def largest_red_and_green(f: TextIOBase) -> int:
         # It's not like there will be 3, 4 or more verticies in a row or column
 
         # This is the first point of the shape
-        x, y = vx.pop(0), vy.pop(0)
-        shape: list[TUPLE2] = [(x, y)]
+        v = verticies.pop(0)
+        shape: list[Tuple2] = [v]
 
         # repeat finding the points on the shape until it is empty
-        while vx:
+        while verticies:
             # Find another node with the same x, or y coordinate
-            nx, ny = find_first(vx, x), find_first(vy, y)
+            n = find_first(verticies, (x, y))
 
             # Start with this as the first point of the shape
-            current_shape = [(nx, ny)]
-            match (nx, ny):
-                case (None, None):
-                    # The new point is not on the shape.
-                    # proceed
+            if n is None:
+                # The point wasn't found. End the shape
+                if len(shape) >= 2:
+                    shapes.append(shape)
+                    # make sure the shape is closed
+                    xstart, ystart = shape[0]
+                    xend, yend = shape[-1]
+                    assert xstart == xend or ystart == yend, "The shape isn't closed"
+                shape = []
+                # Go to next shape
+                break
+            else:
+                # The rows or columns line up. It is part of the shape
+                # Pop it so I don't consider this point again. I know it won't be part of another shape.
+                x, y = vx.pop(n), vy.pop(n)
+                shape.append((x, y))
 
-                    # TODO: At this point, I need to make sure
-                    shape = []
-                    continue
-                case (i, None) | (None, i):
-                    # The rows or columns line up. It is part of the shape
-                    # Pop it so I don't consider this point again
-                    x, y = vx.pop(i), vy.pop(i)
-                    shape.append((x, y))
-                    if x == x_start or y == y_start:
+    if shape:
+        shapes.append(shape)
+    return shapes
 
-                case _:
-                    # Pathological case. This should *never* occur.
-                    # It represents both points coinciding with the last point
-                    assert_never
 
+def get_rotation(verticies: list[Tuple2]) -> int:
+
+    for a, b, c in zip(
+        verticies, verticies[1:] + [verticies[0]], verticies[2:] + [verticies[:2]]
+    ):
+        pass
+
+
+def get_red_green_area(p1: Tuple2, p2: Tuple2, shape: list[Tuple2]) -> int | None:
+    # I have to make sure that the sides of the rectangle don't intersect any of the side of the shape.
+    # It has to fully intersect, just not touch
+    # There are four segments of the square
+    # TODO: Maybe there's a better way to do this...
+    sides = [
+        ((p1[0], p1[1]), (p2[0], p1[1])),
+        ((p2[0], p1[1]), (p2[0], p2[1])),
+        ((p2[0], p2[1]), (p1[0], p2[1])),
+        ((p1[0], p2[1]), (p1[0], p1[1])),
+    ]
+
+
+def largest_red_and_green(f: TextIOBase) -> int:
+    """Compute the largest area consisting entirely of red and green tiles. This is Part 2."""
+
+    # TODO: This function is *really* long. I ought to split it up some.
+    verticies = parse_file(f)
+
+    # split them by x and y coordinates
+    vx = [v[0] for v in verticies]
+    vy = [v[1] for v in verticies]
+
+    shapes = get_shapes(vx, vy)
+
+    # Now I have all the shapes. I need to find the largest rectangle that fits
+    max_area = 0
+    for sh in shapes:
+        # Consider all pairs of points in the shape (rectangles)
+        for p1, p2 in combinations(sh, 2):
+            # I have to make sure that the sides of the rectangle don't intersect any of the side of the shape.
+            # It has to fully intersect, just not touch
+            # There are four segments of the square
+            # TODO: Maybe there's a better way to do this...
+            # here, none of the segments intersect. Compute the area
+            area = get_red_green_area(p1, p2, sh)
+            max_area = max(max_area, area)
+    return max_area
--- test/test_2025/test_2025_day09.py
+++ test/test_2025/test_2025_day09.py
@@ -1,5 +1,7 @@
 from io import StringIO
-from advent_of_code.year_2025.day_09.day_09 import largest_red_rectangle, compute_area, largest_red_and_green
+from advent_of_code.year_2025.day_09.day_09 import *
+import pytest as pt
+from pathlib import Path
 
 test_case = """
 7,1
@@ -10,22 +12,72 @@ test_case = """
 2,5
 2,3
 7,3
-"""
+""".lstrip(
+    "\n"
+)
+
 
 def test_compute_area():
-    assert compute_area((2, 5), (9, 7)) == 24
-    assert compute_area((7, 1), (11, 7)) == 35
-    assert compute_area((7,3), (2, 3)) == 6
-    assert compute_area((2, 5), (11, 1)) == 50
-
+    assert Tuple2(2, 5).compute_area(Tuple2(9, 7)) == 24
+    assert Tuple2(2, 5).compute_area(Tuple2(9, 7)) == 24
+    assert Tuple2(7, 1).compute_area(Tuple2(11, 7)) == 35
+    assert Tuple2(7, 3).compute_area(Tuple2(2, 3)) == 6
+    assert Tuple2(2, 5).compute_area(Tuple2(11, 1)) == 50
 
 
 def test_simple():
-    f = StringIO(test_case.lstrip('\n'))
+    f = StringIO(test_case)
 
     assert largest_red_rectangle(f) == 50
 
-def test_part2():
-    f = StringIO(test_case.lstrip('\n'))
 
-    assert largest_red_and_green(f) == 24
+def test_get_shapes():
+    f = StringIO(test_case)
+
+    verticies = parse_file(f)
+    vx = [x.x for x in verticies]
+    vy = [x.y for x in verticies]
+
+    expected_shape = [
+        [(11, 1), (11, 7), (9, 7), (9, 5), (2, 5), (2, 3), (7, 3), (7, 1)]
+    ]
+    shapes = get_shapes(vx, vy)
+    assert shapes == expected_shape
+    sh = shapes[0]
+
+
+def test_shapes_real_data():
+    # This test tells me the size of the shape of the real data. It's helpful because if it isn't too big, I can use a bytearray to hold the rows/columns
+    fname = (
+        Path(__file__).parent.parent.parent
+        / "advent_of_code"
+        / "year_2025"
+        / "day_09"
+        / "input.txt"
+    )
+
+    with open(fname) as f:
+        verticies = parse_file(f)
+        f.seek(0)
+
+        largest_red = largest_red_rectangle(f)
+
+    vx = [x.x for x in verticies]
+    vy = [x.y for x in verticies]
+
+    assert largest_red == 4777816465
+
+    shapes = get_shapes(vx, vy)
+    assert len(shapes) == 1
+
+
+def test_segment_horizontal():
+    assert segment_horizontal([(0, 0), (10, 0)]) == True
+    assert segment_horizontal([(5, -5), (5, 5)]) == False
+    with pt.raises(AssertionError):
+        segment_horizontal([[1, 2], [3, 4]])
+
+
+def test_get_red_green_area():
+    shapes = [[(11, 1), (11, 7), (9, 7), (9, 5), (2, 5), (2, 3), (7, 3), (7, 1)]]
+    assert True
