1d8ab87797a00b6453906368a5812cb537e030df
David Walker <dbwalker0min@gmail.com>
2025-12-17 20:05:19 -0800

    More Day 11

--- advent_of_code/year_2025/day_11/day_11.py
+++ advent_of_code/year_2025/day_11/day_11.py
@@ -1,5 +1,17 @@
 from io import TextIOBase
 from dataclasses import dataclass, field
+from enum import IntFlag
+
+class FlagNode(IntFlag):
+    NONE = 0
+    FFT = 1
+    DAC = 2
+    ALL = 3
+
+@dataclass
+class PathResults:
+    npaths: int
+    flags: FlagNode
 
 @dataclass
 class TreeNode:
@@ -18,10 +30,17 @@ def parse_data(f: TextIOBase) -> dict[str, TreeNode]:
         tree[nodeid] = TreeNode(nodeid, children)
     return tree
 
-def find_paths(tree: dict[str, TreeNode], node: str) -> int:
+def find_paths(tree: dict[str, TreeNode], node: str, flags_in: FlagNode = FlagNode.NONE, check_nodes: bool = False) -> int:
+    flags = flags_in
+    if node == 'fft':
+        flags |= FlagNode.FFT
+    if node == 'dac':
+        flags |= FlagNode.DAC
     if node == 'out':
         return 1
-    return sum(find_paths(tree, c) for c in tree[node].children)
+    result = find_paths(tree, c, flags) for c in tree[node].children
+    npaths =
+    return 
 
 def find_paths_f(f: TextIOBase) -> int:
     tree = parse_data(f)
