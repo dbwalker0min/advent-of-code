8eebb207216a382f290c5cc5aef860b00887976b
David Walker <dbwalker0min@gmail.com>
2026-01-24 10:34:37 -0800

    2015/7 continuing

--- advent_of_code/year_2015/day_07/day_07.py
+++ advent_of_code/year_2015/day_07/day_07.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Any
 import re
 
 def op_not(x: int, y: int):
@@ -24,7 +24,7 @@ class LogicNode:
     # the arguments can contain a string which is the variable to be substituted.
     arg1: int | str | None = None
     arg2: int | str | None = None
-    out_var: str = None
+    out_var: str | None = None
 
 class LogicKit:
     def __init__(self):
@@ -36,16 +36,16 @@ class LogicKit:
         if m := re.fullmatch(r'(\d+) -> (\w+)', s):
             # This is a literal. Set the variable to the proper value
             value, var = m.groups()
-            self._vars[var] = value
-        elif m := re.fullmatch('NOT (\w+) -> (\w+)', s):
+            self._vars[var] = int(value)
+        elif m := re.fullmatch(r'NOT (\w+) -> (\w+)', s):
             var_i, var_o = m.groups()
             if var_i in self._vars:
                 # it's already defined. Just set the variable
-                self._vars[var_o] = op_not(self._vars[var_i])
+                self._vars[var_o] = op_not(self._vars[var_i], 0)
             else:
                 # the input variable isn't defined (yet)
-                self._wiring.append(LogicNode(op=op_not, arg1=var_i, arg2=0, var_o=var_o))
-        elif m := re.fullmatch(r'(\w+|\d+) (\w+) (\w+|\d+) -> (\w+)'):
+                self._wiring.append(LogicNode(op=op_not, arg1=var_i, arg2=0, out_var=var_o))
+        elif m := re.fullmatch(r'(\w+|\d+) (\w+) (\w+|\d+) -> (\w+)', s):
             # This is a two input function
             a1, op, a2, out = m.groups()
 
